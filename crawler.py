bb9a75f7fd7799efe55af2c1bdcecd37ea199271
Xyphuz <hansen0715.2@gmail.com>
2021-06-01 02:06:39 +0800

    Add emoji_amount and reply_amount.

--- crawler.py
+++ crawler.py
@@ -14,6 +14,12 @@ def crawl():
     link = driver.find_element_by_xpath("/html/body/div[1]/div[2]/div[2]/div/div/div/div/div[2]/div/div[1]/div[3]/article/h2/a")
     print(link.get_attribute("href"))
 
+    emoji_amount = driver.find_element_by_xpath("/html/body/div[1]/div[2]/div[2]/div/div/div/div/div[2]/div/div[1]/div[3]/article/div[3]/div[1]/div/div[2]")
+    print("emoji_amount:", emoji_amount.text)
+
+    reply_amount = driver.find_element_by_xpath("/html/body/div[1]/div[2]/div[2]/div/div/div/div/div[2]/div/div[1]/div[3]/article/div[3]/div[2]/div/span")
+    print("reply_amount:", reply_amount.text)
+
     driver.quit()
 
 if __name__ == "__main__":
