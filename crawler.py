744aa76aff3ebf993e105926045884bf2e628942
Xyphuz <hansen0715.2@gmail.com>
2021-06-01 00:28:38 +0800

    Add title and link.

--- crawler.py
+++ crawler.py
@@ -8,6 +8,12 @@ def crawl():
     driver = webdriver.Chrome(options=chrome_options)
     driver.get("https://www.dcard.tw/f")
 
+    title = driver.find_element_by_xpath("/html/body/div[1]/div[2]/div[2]/div/div/div/div/div[2]/div/div[1]/div[3]/article/h2/a/span")
+    print(title.text)
+
+    link = driver.find_element_by_xpath("/html/body/div[1]/div[2]/div[2]/div/div/div/div/div[2]/div/div[1]/div[3]/article/h2/a")
+    print(link.get_attribute("href"))
+
     driver.quit()
 
 if __name__ == "__main__":
